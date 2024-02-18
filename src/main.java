7aff60f9355b55bdce094ce2d6f3db32cf5e56c7
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2024-02-18 19:06:42 -0300

    Desafios 6 e 7

--- src/main.java
+++ src/main.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -13,7 +12,7 @@ public class main {
 
         // Desafio 02 - Imprima a soma dos números pares da lista:
         Predicate<Integer> isPar = numero -> numero % 2 == 0;
-        BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+        BinaryOperator<Integer> somar = Integer::sum;
 
         List<Integer> listaNumerosPares = numeros.stream().filter(isPar).toList(); // Desafio 04 - Remova todos os valores ímpares:
         int resultadoSoma = listaNumerosPares.stream().reduce(0, somar);
@@ -23,18 +22,32 @@ public class main {
 
         List<Integer> listaNumerosPositivos = numeros.stream()
         .filter(isPositivo)
-        .collect(Collectors.toList());
+        .toList();
 
         // Desafio 05 - Calcule a média dos números maiores que 5:
         Predicate<Integer> isMaior5 = numero -> numero > 5;
 
         List<Integer> listaNumerosMaior5 = numeros.stream().filter(isMaior5).toList();
 
+        // Desafio 6 - Verificar se a lista contém algum número maior que 10:
+        boolean isMaior10 = numeros.stream().allMatch(numero -> numero > 10);
+
+        // Desafio 7 - Encontrar o segundo número maior da lista:
+        Integer segundoMaiorNumero = listaNumerosOrdenados.stream()
+                .distinct() // Retirar números repetidos
+                .sorted(Comparator.reverseOrder()) // Invertendo a ordem
+                .skip(1) // Pulando o primeiro número (maior da lista)
+                .findFirst() // Pegando o próximo número
+                .orElse(null); // Retorna null caso não encontre nenhum número
+
+
         System.out.println("Lista ordem numérica: " + listaNumerosOrdenados);
         System.out.println("A soma dos números pares é: " + resultadoSoma);
         System.out.println("Lista dos números positivos: " + listaNumerosPositivos);
         System.out.println("Lista de números pares: " + listaNumerosPares);
         System.out.println("Lista dos números maiores do que 5: " + listaNumerosMaior5);
+        System.out.println("Existem números na lista maiores do que 10? " + isMaior10);
+        System.out.println("Segundo maior número da lista" + segundoMaiorNumero);
 
     }
 }
