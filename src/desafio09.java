44f9b691991d0d715875b53aced21fcca5bc008b
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 20:05:59 -0300

    fix: atendendo aos requisitos do desafio 09

---
+++ src/desafio09.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class desafio09 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 9 - Verificar se todos os números da lista são distintos (não se repetem):
+        boolean todosDistintos = numeros.stream()
+                .distinct() // Retira os números repetidos
+                .count() == numeros.size(); // Verifica se o número de elementos é igual ao número de elementos da lista original
+        System.out.println("Todos os números da lista são distintos? " + todosDistintos);
+    }
+}
