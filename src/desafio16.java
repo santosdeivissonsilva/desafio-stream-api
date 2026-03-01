62aa0bfaa4f822f33434c24f48dede1d046e9e75
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 20:51:31 -0300

    fix: atendendo aos requisitos do desafio 1

---
+++ src/desafio16.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class desafio16 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 16 - Agrupar os números em pares e ímpares:
+        List<Integer> listaPares = numeros.stream()
+                .filter(numero -> numero % 2 == 0) // Filtra os números que são pares
+                .toList();
+        List<Integer> listaImpares = numeros.stream()
+                .filter(numero -> numero % 2 != 0) // Filtra os números que são ímpares
+                .toList();
+
+        System.out.println("Lista de números pares: " + listaPares + " e lista de números ímpares: " + listaImpares);
+    }
+}
