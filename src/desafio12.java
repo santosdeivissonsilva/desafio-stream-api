1aa64c24e3d887c034ee597f2740b65899a075f9
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 20:27:55 -0300

    fix: atendendo aos requisitos do desafio 12

---
+++ src/desafio12.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class desafio12 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 12 - Encontrar o produto de todos os números da lista:
+        int produto = numeros.stream()
+                .reduce(1, (a, b) -> a * b); // Multiplica todos os números
+        System.out.println("Produto de todos os números da lista: " + produto);
+    }
+}
