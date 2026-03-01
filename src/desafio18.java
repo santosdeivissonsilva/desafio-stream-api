050024ed5ea0bc62fec4c70ec9e9db3da9ecaa36
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 20:51:44 -0300

    fix: atendendo aos requisitos do desafio 18

---
+++ src/desafio18.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class desafio18 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 18 - Verificar se todos os números da lista são iguais:
+        boolean todosIguais = numeros.stream()
+                .allMatch(numero -> numero == numeros.get(0)); // Verifica se todos os números são iguais ao primeiro número da lista
+                
+        System.out.println("Todos os números da lista são iguais? " + todosIguais);
+    }
+}
