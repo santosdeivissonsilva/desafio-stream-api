9d352b04e8bd62d1886100d9c6f3a9fff52a836c
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 20:05:35 -0300

    fix: atendendo aos requisitos do desafio 06

---
+++ src/desafio06.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class desafio06 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 6 - Verificar se a lista contém algum número maior que 10:
+        Predicate<Integer> isMaior10 = numero -> numero > 10;
+        boolean contemMaior10 = numeros.stream()
+                .filter(isMaior10) // Filtra os números que são maiores que 10
+                .count() > 0; // Verifica se a lista contém algum número maior que 10
+        System.out.println("A lista contém algum número maior que 10? " + contemMaior10);
+    }
+}
