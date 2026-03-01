d3d6f5edece952ca8ea1802539ef09b0ff0bb9f4
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 19:37:46 -0300

    fix: atendendo aos requisitos do desafio 04

---
+++ src/desafio04.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class desafio04 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 04 - Remova todos os valores ímpares:
+        List<Integer> listaNumerosPares = numeros.stream()
+                .filter(numero -> numero % 2 == 0) // Filtra os números que são pares
+                .toList(); // Converte o stream para uma lista
+
+        List<Integer> listaNumerosImpares = numeros.stream()
+                .filter(numero -> numero % 2 != 0) // Filtra os números que são ímpares
+                .toList(); // Converte o stream para uma lista
+
+        System.out.println("Lista de números pares: " + listaNumerosPares);
+        System.out.println("Lista de números ímpares: " + listaNumerosImpares);
+    }
+}
