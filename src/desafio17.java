59ccca7c76ac698d3e80c0258ed6d67898459e4e
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 20:51:37 -0300

    fix: atendendo aos requisitos do desafio 17

---
+++ src/desafio17.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class desafio17 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 17 - Filtrar os números primos da lista:
+        Predicate<Integer> isPrimo = numero -> {
+            if (numero <= 1) {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.sqrt(numero); i++) {
+                if (numero % i == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        List<Integer> listaPrimos = numeros.stream()
+                .filter(isPrimo) // Filtra os números que são primos
+                .toList();
+
+        System.out.println("Lista de números primos: " + listaPrimos);
+    }
+}
