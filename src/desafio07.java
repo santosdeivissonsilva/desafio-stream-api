6aeb76e6baf4b8fa4e7ebe3de30d1cadfd036479
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2026-03-01 20:05:44 -0300

    fix: atendendo aos requisitos do desafio 07

---
+++ src/desafio07.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class desafio07 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 7 - Encontrar o segundo número maior da lista:
+        Integer segundoMaiorNumero = numeros.stream()
+                .distinct() // Retirar números repetidos
+                .sorted(Comparator.reverseOrder()) // Invertendo a ordem
+                .skip(1) // Pulando o primeiro número (maior da lista)
+                .findFirst() // Pegando o próximo número
+                .orElse(null); // Retorna null caso não encontre nenhum número
+        System.out.println("O segundo número maior da lista é: " + segundoMaiorNumero);
+    }
+}
